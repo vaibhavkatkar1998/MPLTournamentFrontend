6e8a15bf3aecc2c358db93460ec115ecab22a7c7
Vaibhav <vaibhav.katkar2302@gmail.com>
2025-02-13 09:22:09 +0530

    Updated user nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router, NavigationEnd } from '@angular/router';
 import { LoginService } from './login/login.service';
 
 @Component({
@@ -7,21 +8,43 @@ import { LoginService } from './login/login.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'MPLTournament';
-  userRole: any;
-  userName: any;
-  constructor(private loginService : LoginService) {
-    this.userRole = this.loginService.getClaim("role");
-    this.userName = this.loginService.getClaim("sub");
-  }
+  userRole: string | null = null;
+  userName: string | null = null;
+  showNavbar = true;
 
   @ViewChild('sidenav') sidenav!: MatSidenav;
   sidenavOpened = false;
 
+  constructor(private loginService: LoginService, private router: Router) {
+    // Listen for route changes
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = this.router.url !== '/'; // Hide navbar on login page
+        this.updateUserDetails();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.updateUserDetails();
+  }
+
+  updateUserDetails() {
+    this.userRole = this.loginService.getClaim("role");
+    this.userName = this.loginService.getClaim("sub");
+  }
+
   toggleSidenav() {
     this.sidenavOpened = !this.sidenavOpened;
     this.sidenav.toggle();
+  }
+
+  closeSidenav() {
+    if (this.sidenav) {
+      this.sidenav.close();
+    }
   }
 
   logoutFromSystem() {
