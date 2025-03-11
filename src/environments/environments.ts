84a52aa8fc18e9660d27d78a17b28e03f5437ffc
Vaibhav <vaibhav.katkar2302@gmail.com>
2025-03-11 21:26:57 +0530

    test

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production : true,
-    endPoint: 'http://localhost:8080/'
+    endPoint: 'http://mpl-tournament.eu-north-1.elasticbeanstalk.com/'
+    // endPoint: 'http://locolhost:8080/'
 }
